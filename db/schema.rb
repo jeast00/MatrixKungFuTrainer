5b36dace140a81d381ae2067f3934e6a32ded584
Jonathan East <jonathan_east@ymail.com>
2020-08-12 16:04:49 -0400

    migrated and rolled back db tables; removed password and added password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_181203) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
