ddf9e4f2a30b6d929aab68fd9aa6ebd0f3a99f2e
Jonathan East <jonathan_east@ymail.com>
2020-08-13 15:54:58 -0400

    Moved users_controller.rb code (sessions) into sessions_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,5 @@
 class UsersController < ApplicationController
 
-  get '/registrations/signup' do
-    erb :'registrations/signup'
-  end
-
-  post '/registrations' do
-    @user = User.create(params)
-    # @user.save
-    session[:user_id] = @user.id
-    erb :'users/homepage'
-  end
-
-  get '/sessions/login' do
-    erb :'sessions/login'
-  end
-
-  post '/sessions' do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      erb :'users/homepage'
-    else
-      erb :'sessions/login'
-    end
-  end
-
-
-  get '/users/homepage' do
-    @user = User.find(session[:user_id])
-    erb :'users/homepage'
-  end
+  
 
 end
