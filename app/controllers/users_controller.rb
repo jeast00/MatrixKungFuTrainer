d3844d08fbed19d455a57780bee8d1bb0d1caa97
Jonathan East <jonathan_east@ymail.com>
2020-08-12 20:07:04 -0400

    Added get request for sessions/login; added a post request for sessions; added a get request for users/homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,23 @@ class UsersController < ApplicationController
     erb :'users/homepage'
   end
 
+  get '/sessions/login' do
+    erb :'sessions/login'
+  end
+
+  post '/sessions' do
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/homepage'
+    end
+    redirect '/sessions/login'
+  end
+
+
+  get '/users/homepage' do
+    @user = User.find(session[:user_id])
+    erb :'users/homepage'
+  end
+
 end
